d4a92440e499fdeba61343ba1b561ca8c37cd8d5
MrWillian <willian.dev10@gmail.com>
2023-01-16 15:58:23 -0300

    feat(pages): set index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,93 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { ChevronDownIcon, DotsVerticalIcon, PlusCircleIcon, PlusIcon } from '@heroicons/react/outline';
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import Layout from './layout';
+import CardItem from '../components/CardItem';
+import BoardData from '../data/board-data.json';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <Layout>
+      <div className="p-10">
+        {/* Board header */}
+        <div className="flex justify-between">
+          <div className="flex items-center">
+            <h4 className="text-4xl font-bold text-gray-600">Studio Board</h4>
+            <ChevronDownIcon className="w-9 h-9 text-gray-500 rounded-full p-1 bg-white ml-5 shadow-xl" />
+          </div>
+          <ul className="flex space-x-3">
+            <li>
+              <Image 
+                src="https://randomuser.me/api/portraits/men/75.jpg" 
+                width="36" 
+                height="36" 
+                objectFit='cover' 
+                className=" rounded-full " 
+                alt="image"
+              />
+            </li>
+            <li>
+              <Image 
+                src="https://randomuser.me/api/portraits/men/76.jpg" 
+                width="36" 
+                height="36" 
+                objectFit='cover' 
+                className=" rounded-full " 
+                alt="image"
+              />
+            </li>
+            <li>
+              <Image 
+                src="https://randomuser.me/api/portraits/men/77.jpg" 
+                width="36" 
+                height="36" 
+                objectFit='cover' 
+                className=" rounded-full " 
+                alt="image"
+              />
+            </li>
+            <li>
+              <button className="border border-dashed flex items-center w-9 h-9 border-gray-500 justify-center rounded-full">
+                <PlusIcon className="w-5 h-5 text-gray-500" />
+              </button>
+            </li>
+          </ul>
         </div>
-      </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+        {/* Board columns */}
+        <div className="grid grid-cols-4 gap-5 my-5">
+          {
+            BoardData.map((board, index) => {
+              return (
+                <div key={index} className="bg-gray-100 p-3 rounded-md shadow-md flex flex-col relative overflow-hidden">
+                  <span className="w-full h-1 bg-gradient-to-r from-pink-700 to-red-200 absolute inset-x-0 top-0"></span>
+                  <h4 className="flex justify-between items-center mb-2">
+                    <span className="text-2xl text-gray-600">{board.name}</span>
+                    <DotsVerticalIcon className='w-5 h-5 text-gray-500' />
+                  </h4>
+
+                  {
+                    board.items.length > 0 && (
+                      board.items.map((item, iIndex) => {
+                        return <CardItem key={iIndex} data={item} />
+                      })
+                    )
+                  }
+
+                  <button className="flex justify-center items-center mt-6 space-x-2 text-lg">
+                    <span>Add task</span>
+                    <PlusCircleIcon className="w-4 h-4 text-gray-500" />
+                  </button>
+
+                </div>
+              )
+            })
+          }
+        </div>
+        
+      </div>
+
+    </Layout>
   )
 }
 
